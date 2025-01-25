2419148d743705ba4d7fbde06e6552bb46777a63
ASF <Doum79>
2025-01-25 18:11:54 +0100

    udpate

--- PeopleManager/PeopleManager.Domain/obj/Debug/net8.0/PeopleManager.Domain.AssemblyInfo.cs
+++ PeopleManager/PeopleManager.Domain/obj/Debug/net8.0/PeopleManager.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeopleManager.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa6b6ccb63e41329ab5d8d2aa745d0936aa1a03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e6ec0d6c064714acc3a49a54137a5c9a93a2c54")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeopleManager.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeopleManager.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
